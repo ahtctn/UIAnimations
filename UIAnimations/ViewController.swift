7e181fc31cd205aa186390f73e257189851af30d
Ahmet Ali Çetin <ahtctn@gmail.com>
2023-03-15 16:41:42 +0300

    basic uiview animates

--- UIAnimations/ViewController.swift
+++ UIAnimations/ViewController.swift
@@ -9,9 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: OUTLETS
+    @IBOutlet weak var animatableView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+//        UIView.animate(withDuration: 1) {
+//            self.animatableView.backgroundColor = .red
+//            self.animatableView.frame.size = CGSize(width: 100, height: 100)
+//            self.animatableView.frame = CGRect(x: 100, y: 100, width: 70, height: 40)
+//        }
+        
+        UIView.animate(withDuration: 2) {
+            self.animatableView.backgroundColor = .red
+            self.animatableView.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        } completion: { _ in
+            UIView.animate(withDuration: 1) {
+                self.animatableView.backgroundColor = .systemPurple
+                self.animatableView.frame = CGRect(x: 100, y: 100, width: 100, height: 700)
+            }
+        }
+
     }
 
 
